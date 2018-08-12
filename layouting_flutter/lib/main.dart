d9a3604e988858fc001eea7e5b63697c565dde33
Asep Septiadi <mcreator.id.dev@gmail.com>
2018-08-12 09:49:10 +0700

    Update #2 Bag. 1
    layouting_basic_in_flutter

---
+++ layouting_flutter/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: LayoutingFlutter(),
+  ));
+}
+
+class LayoutingFlutter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Layouting in Flutter"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Image.network("https://flutter.io/images/homepage/header-illustration.png")
+        ],
+      ),
+    );
+  }
+}
